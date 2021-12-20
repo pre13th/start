91d9fa5a86ca0a99f75762c64b3ba44a44381c34
matty255 <matt14507@gmail.com>
2021-12-20 15:51:09 +0900

    커넥트!

---
+++ static/move.js
@@ -0,0 +1,17 @@
+
+        $(document).ready(function () {
+
+        });
+
+        function home() {
+            window.location.href = '/';
+        };
+
+        function star() {
+            window.location.href = '/star';
+        };
+
+        function about() {
+            window.location.href = '/about';
+        };
+        
