29abbccebbc4870ec68d70e55245aa85be05f608
Jaemin Kim <clsne0328@daum.net>
2021-12-27 00:09:35 +0900

    second commit

--- static/move.js
+++ static/move.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-        gift_order();
+       getgarland();
 });
+
 
 
 
@@ -21,41 +22,3 @@ function about() {
     window.location.href = '/about';
 }
 
-function gift_order() {
-                $.ajax({
-                    type: 'GET',
-                    url: '/order',
-                    data: {},
-                    success: function (response) {
-                        let mygift = response['gift_order']
-                        for (let i = 0; i <mygift.length; i++){
-                               let image = mygift[i]['image']
-                               let desc = mygift[i]['desc']
-                               let title = mygift[i]['title']
-                               let price = mygift[i]['price']
-                               let delivery_fee = mygift[i]['delivery_fee']
-                               let like = mygift[i]['like']
-                               let review = mygift[i]['review']
-                               let url = mygift[i]['url']
-
-                               let temp_html = `<div class="card long" style="width: 18rem;">
-           
-            <img class="card-img-top"
-                 src="${image}"
-                 alt="Card image cap">
-            <div class="card-body">
-            <a href="${url}" class="btn btn-primary">${title} (: ${like}</a>
-                <p class="card-text">${desc}</p>
-              
-                <span class="card-text price">${price}</span>
-                 <span class="card-text price">${delivery_fee} </span>
-                     <p class="card-text review2">(리뷰: ${review})</p>             
-            </div></div>
-            `
-                            $('#gift-cards').append(temp_html)
-
-                        }
-
-                    }
-                });
-}
